1ac1205ad26cdc9bb35e69c40553a90cb81691e4
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-07-29 15:02:37 -0400

    did what I could without code along

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 const Ray = function() {
-  
+    const ray = {
+        length: 0,
+
+        push: function(value){
+            
+            this[this.length] = value
+            this.length++;
+
+
+        },
+
+       pop: function(){
+        
+        this.length--
+
+        
+
+    
+       }
+
+
+    }
+
+
+
+
+  return ray
 }
+
+
+
+
 
 
 module.exports = Ray
